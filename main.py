71a79ea2c3b762ac34a043222507febbee46f595
Chadis16 <chad.moore.cm15@gmail.com>
2025-08-13 16:19:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -222,7 +222,9 @@ def Categories():
 @app.route('/BalanceTracking', methods=['POST','GET'])
 def BalanceTracking():
     global finance_app
+    global username
     finance_app = finance_app
+    username = username
     app.logger.debug("This is a debug message from the index route.")
     BalanceTrack = Balance_Tracking()
     account = Account()
@@ -459,6 +461,7 @@ def Investment():
 if __name__ == '__main__':
 
     app.run(host='0.0.0.0')
+
 
 
 
