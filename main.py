daf596c7af7ceeaff70470b56e0e294dc1fe9e54
Chadis16 <chad.moore.cm15@gmail.com>
2025-08-19 21:24:07 +0000

    fix bud table

--- main.py
+++ main.py
@@ -567,10 +567,11 @@ def Budget():
     accttran['Amount'] = accttran['Amount'].apply(lambda x: f"${x:,.2f}")
     transactions = transactions.drop(columns=['Year','Month'])
     transactions = transactions.fillna('Total')
-    transactions = transactions.to_html(escape=False,index=False,table_id='Budget')
     accttran = accttran.drop(columns=['Year','Month'])
     accttran = accttran.fillna('Total')
+    transactions = transactions.to_html(escape=False,index=False,table_id='Budget')
     accttran = accttran.to_html(escape=False,index=False,table_id='accttran')
+    bud = bud.to_html(escape=False,index=False,table_id='bud')
     return render_template('budget.html',transactions=transactions,mon=mon,year=y,monstr=monstr,accttran = accttran,income = income,bud=bud)
 
 @app.route('/Investments/')
