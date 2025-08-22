e58f5391e6c916972116165fe0e8e30654974ebd
Chadis16 <chad.moore.cm15@gmail.com>
2025-08-22 19:48:47 +0000

    fixed it probably

--- main.py
+++ main.py
@@ -651,7 +651,7 @@ def debts():
     finance_app = engine.connect()
     debts = pd.read_sql('SELECT * from debts;',finance_app)
     debts['Edit'] = debts['iddebts'].apply(lambda x: f'<button onclick="editdebt({x},\'{eddebt(x,'Account',debts)}\',\'{eddebt(x,'Interest Rate',debts)}\',\'{eddebt(x,'Min Payment',debts)}\',\'{eddebt(x,'Due Date',debts)}\')">Edit</button>')
-    debts['Mark Paid'] = debts['iddebts'].apply(lambda x: f'<form method="POST" action="/debtpaid/"><input type="hidden" id="ID" value="{x}"><input type="hidden" id="DDate" name="DDate" value="{DueDate}"><button type="submit">Mark Paid</button></form>')
+    debts['Mark Paid'] = debts['iddebts'].apply(lambda x: f'<form method="POST" action="/debtpaid/"><input type="hidden" id="ID" value="{x}"><input type="hidden" id="DDate" name="DDate" value="{eddebt(x,'Due Date',debts}"><button type="submit">Mark Paid</button></form>')
     debts = debts.drop(columns=['iddebts'])
     finance_app.close()
     return debts
